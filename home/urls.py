8760ee003f87664e685f8993e58570fb7988bca0
Oleksiy Lyashenko <oleksij.lyashenko@gmail.com>
2023-08-04 23:45:15 +0300

    Create Task List in views and urls

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import index
+from .views import index, TaskListView
 
 urlpatterns = [
     path('', index, name='index'),
+    path('task/', TaskListView.as_view(), name="task-list")
 ]
+
+app_name = "home"
--- home/views.py
+++ home/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+
+from home.models import Task
 
 
 def index(request):
 
     # Page from the theme 
     return render(request, 'pages/index.html')
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    template_name = "home/task_list.html"
