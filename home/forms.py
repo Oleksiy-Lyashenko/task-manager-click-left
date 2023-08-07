b7787e4c7a747eedba1a32aa5f5fecab918042e4
Oleksiy Lyashenko <oleksij.lyashenko@gmail.com>
2023-08-07 16:07:59 +0300

    Change logic context in WorkerDetailView

--- home/forms.py
+++ home/forms.py
@@ -1,10 +1,22 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django.core.validators import RegexValidator
+
 from home.models import Worker, Task
 
 
 class WorkerCreationForm(UserCreationForm):
+    username = forms.CharField(
+        max_length=83,
+        required=True
+    )
+
+    first_name = forms.CharField(
+        max_length=83,
+        required=True
+    )
+
     class Meta(UserCreationForm.Meta):
         model = Worker
         fields = UserCreationForm.Meta.fields + (
@@ -30,13 +42,14 @@ class TaskForm(forms.ModelForm):
     assignees = forms.ModelMultipleChoiceField(
         queryset=get_user_model().objects.all(),
         widget=forms.CheckboxSelectMultiple,
-        required=False
+        required=True
     )
     deadline = forms.DateTimeField(
-        input_formats="%m-%d-%y",
+        input_formats=["%Y-%m-%d %H:%M"],
         widget=forms.TextInput(
-            attrs={'placeholder': '21-10-2001'}
-        )
+            attrs={'placeholder': '2000-01-01 16:00'}
+        ),
+        required=True
     )
 
     class Meta:
