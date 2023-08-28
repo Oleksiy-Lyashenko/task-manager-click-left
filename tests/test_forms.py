ad03fee7e99df0d397f6d0b92dc02078e25be577
Oleksiy Lyashenko <oleksij.lyashenko@gmail.com>
2023-08-28 21:45:18 +0300

    In TaskForm change assignees filed to from MultipleChoiceField to ModelChoiceField

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -1,10 +1,10 @@
 from django.contrib.auth import get_user_model
-from django.forms import ModelChoiceField
+from datetime import datetime
 from django.test import TestCase
 from django.urls import reverse
 
 from home.forms import WorkerCreationForm
-from home.models import Position, Worker, TaskType
+from home.models import Position, Worker, TaskType, Task
 
 
 class FormTests(TestCase):
@@ -24,25 +24,23 @@ class FormTests(TestCase):
             "username": "mac_username",
             "first_name": "Mac First",
             "last_name": "Mac Last",
-            # "position": 1,
+            "position": self.position,
             "password1": "Qwerty1234567",
             "password2": "Qwerty1234567",
         }
 
-        # Worker.objects.create(
-        #     username="mac_username",
-        #     first_name="First",
-        #     last_name="Last",
-        #     position=self.position,
-        #     password="Qwerty1234567",
-        # )
         form = WorkerCreationForm(data=form_data)
         self.assertTrue(form.is_valid())
 
         response = self.client.post(reverse('home:worker-create'), data=form_data)
-        new_worker = get_user_model().objects.all()
-        # #
-        a = 5
+        new_worker = get_user_model().objects.select_related("position").get(
+            username=form_data["username"]
+        )
+
+        self.assertEqual(new_worker.first_name, form_data["first_name"])
+        self.assertEqual(new_worker.last_name, form_data["last_name"])
+        self.assertEqual(new_worker.username, form_data["username"])
+        self.assertEqual(new_worker.position, form_data["position"])
 
     def test_search_form_is_work_in_position_list(self):
         text_to_filter = "QA"
@@ -133,31 +131,46 @@ class FormTests(TestCase):
             list(positions_to_filter)
         )
 
-    def test_search_form_is_work_in_worker_list(self):
-        text_to_filter = "User1"
+    def test_search_form_is_work_in_task_list(self):
+        text_to_filter = "Modify page"
         data = {
             "name": text_to_filter
         }
 
-        Worker.objects.create(
-            username="user1",
-            password="pass12345678",
-            first_name="User1",
-            last_name="Last",
+        task_type1 = TaskType.objects.create(name="Create site")
+        task_type2 = TaskType.objects.create(name="Create app")
+        task1 = Task.objects.create(
+            name="Modify page",
+            description="Create home page to site",
+            deadline=datetime(2024, 8, 25),
+            is_completed=False,
+            priority="Normal",
+            task_type=task_type1,
         )
-        Worker.objects.create(
-            username="user2",
-            password="pass12345678",
-            first_name="User2",
-            last_name="Last",
+        task2 = Task.objects.create(
+            name="Create app",
+            description="Create app for pizza company",
+            deadline=datetime(2025, 8, 25),
+            is_completed=False,
+            priority="Normal",
+            task_type=task_type2,
         )
 
-        response = self.client.get(reverse("home:worker-list"), data=data)
+        task1.assignees.add(self.user)
+        task2.assignees.add(self.user)
 
-        positions_after_filter_on_page = response.context_data["worker_list"]
-        positions_to_filter = Worker.objects.filter(first_name=data["name"])
+        response = self.client.get(reverse("home:task-list"), data=data)
+
+        tasks_after_filter_on_page = response.context_data["task_list"]
+        tasks_to_filter = Task.objects.select_related(
+            "task_type"
+        ).prefetch_related(
+            "assignees"
+        ).filter(
+            name=data["name"]
+        )
 
         self.assertEqual(
-            list(positions_after_filter_on_page),
-            list(positions_to_filter)
+            list(tasks_after_filter_on_page),
+            list(tasks_to_filter)
         )
